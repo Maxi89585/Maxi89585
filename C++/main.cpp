5c4229f7a331d04f2aeb408ac8513a8fdedc35d5
Maxi89585 <maxi89585@gmail.com>
2022-04-24 23:47:25 -0500

    Updated

---
+++ C++/main.cpp
@@ -0,0 +1,16 @@
+
+
+int main(){
+    int num1;
+    int num2;
+    int sum;
+
+    out<<"Please enter two numbers separatedby a space:"<<endl;
+    in>>num1>>num2;
+
+    sum = num1 + num2;
+
+    out<<num1<<" + "<<num2<<" = "<<sum<<endl;
+
+    return 0;
+}
